22d9f52df025533e50055728b38836d697a015a9
jioneee <qoxx3659@gmail.com>
2023-06-10 21:14:52 +0900

    Design: 장바구니 틀

--- src/pages/ShoppingCart.tsx
+++ src/pages/ShoppingCart.tsx
@@ -1,13 +1,97 @@
 import styled from "styled-components"
 
-const CartBox = styled.div`
+const CartContainer = styled.div`
     
 `
+const CartHeader = styled.div`
+    box-sizing: border-box;
+    border-bottom: 1px solid gray;
+    margin-top:50px;
+    padding: 10px;
+    text-align: left;
+    font-size: 30px;
+`
+const Cartcontents = styled.div`
+    box-sizing: border-box;
+    border-top: 3px solid black;
+    border-bottom: 3px solid black;
+    margin-top:50px;
+    width: 900px;
+    height: 70px;
+`
+
+const CartContentTitle = styled.div`
+    box-sizing: border-box;
+    text-align: left;
+    height:50px;
+    border-bottom: 2px solid black;
+    background-color: #e3e0e0;
+`
+const CartContent = styled.div`
+    box-sizing: border-box;
+    height:100px;
+`
+const  DeleteSelection = styled.button`
+    display: flex;
+    margin-top: 30px;
+    width: 100px;
+    height: 30px;
+    align-items: flex-start;
+`
+const TotalBox = styled.div`
+    box-sizing: border-box;
+    border-top: 3px solid #e3e0e0;
+    border-bottom: 3px solid #e3e0e0;
+    margin-top:50px;
+    width: 900px;
+    height: 100px;
+`
+const ButtonBox = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-top: 30px;
+`
+
+const SelectionOrder = styled.button`
+    display: flex;
+    margin: 10px;
+    width: 100px;
+    height: 30px; 
+`
+const  TotalOrder = styled.button`
+    display: flex;
+    margin: 10px;
+    width: 100px;
+    height: 30px;
+
+`
+
 
 const ShoppingCart = () => {
     return (
         <div>
-            <CartBox>cart</CartBox>
+            <CartContainer>
+           
+                <CartHeader>
+                장바구니               
+                </CartHeader>
+                <Cartcontents>
+                    <CartContentTitle>
+                        상품옵션
+                    </CartContentTitle>
+                    <CartContent></CartContent>
+
+                </Cartcontents>
+                <DeleteSelection>선택 삭제</DeleteSelection>
+                <TotalBox></TotalBox>
+                <ButtonBox>
+                    <SelectionOrder>선택 주문</SelectionOrder>
+                    <TotalOrder>전체 주문</TotalOrder>
+                </ButtonBox>
+               
+                
+                </CartContainer>
+          
         </div>
     )
 }
