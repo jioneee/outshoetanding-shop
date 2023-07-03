69b769b88516237bfa5dbadab995e0ba536afc8c
jioneee <qoxx3659@gmail.com>
2023-07-03 22:30:13 +0900

    Feat: 기호 변경

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,30 +1,28 @@
 import CopyrightOutlinedIcon from '@mui/icons-material/CopyrightOutlined';
-import styled from "styled-components"
+import styled from 'styled-components';
 
 const FooterBox = styled.div`
-position: relative;
-width:100%;
-height:100px;
-bottom: 0%;
-border: solid white 2px;
-background-color: rgba(0,2,0,10%);;
-`
+  position: relative;
+  width: 100%;
+  height: 100px;
+  bottom: 0%;
+  border: solid white 2px;
+  background-color: rgba(0, 2, 0, 10%);
+`;
 
 const FooterText = styled.span`
-text-align: right;
-color: black;
-`
-
+  text-align: right;
+  color: black;
+`;
 
 const Footer: any = () => {
-    return (
+  return (
     <footer>
-        <FooterBox>
-        <CopyrightOutlinedIcon /><FooterText> outSHOEtanding. All Rights Reserved</FooterText>
-        </FooterBox>
+      <FooterBox>
+        <FooterText> ⓒoutSHOEtanding. All Rights Reserved</FooterText>
+      </FooterBox>
     </footer>
-    )
+  );
+};
 
-}
-
-export default Footer
+export default Footer;
