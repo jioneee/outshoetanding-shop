d523f890e935a1099fe3d40763c49d7855a811e7
jioneee <qoxx3659@gmail.com>
2023-09-03 23:31:10 +0900

    Feat: menu navigate 적용

--- src/components/common/MenuBar.tsx
+++ src/components/common/MenuBar.tsx
@@ -1,11 +1,24 @@
 import styled from 'styled-components';
+import { useNavigate } from 'react-router';
 
 const MenuBar = () => {
+  const navigate = useNavigate();
+  const handleClickHome = () => {
+    navigate('/');
+  };
+
+  const handleClickCart = () => {
+    navigate('/cart');
+  };
+
+  const handleClickMyPge = () => {
+    navigate('/mypage');
+  };
   return (
     <MenuBox>
-      <MenuText>Home</MenuText>
-      <MenuText>Cart</MenuText>
-      <MenuText>MyPage</MenuText>
+      <MenuNavigate onClick={handleClickHome}>Home</MenuNavigate>
+      <MenuNavigate onClick={handleClickCart}>Cart</MenuNavigate>
+      <MenuNavigate onClick={handleClickMyPge}>MyPage</MenuNavigate>
     </MenuBox>
   );
 };
@@ -16,15 +29,17 @@ const MenuBox = styled.div`
   display: flex;
   flex-direction: column;
   box-sizing: border-box;
-  border: 1px solid black;
   background-color: rgba(0, 2, 0, 70%);
   width: 150px;
   height: 150px;
   margin-left: -10px;
+  margin-top: 6px;
+  padding-left: 15px;
 `;
 
-const MenuText = styled.div`
+const MenuNavigate = styled.div`
   color: white;
   font-size: large;
   margin-top: 20px;
+  cursor: pointer;
 `;
