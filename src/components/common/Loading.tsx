ab0d27f2d27a33d59ee3931f40cf787d437bd94c
jioneee <qoxx3659@gmail.com>
2023-11-07 20:40:33 +0900

    Design: 로딩 스타일 적용

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -1,5 +1,30 @@
+import styled from 'styled-components';
+
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <LoadingContainer>
+      <LoadingText>Loading...</LoadingText>
+    </LoadingContainer>
+  );
 };
 
 export default Loading;
+
+const LoadingContainer = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background: #ffffffb7;
+  z-index: 999;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LoadingText = styled.div`
+  font: 1rem 'Noto Sans KR';
+  text-align: center;
+`;
