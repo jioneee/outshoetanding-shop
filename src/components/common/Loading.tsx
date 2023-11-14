df9f1cc464b4468a51d21f107b915b7de1fd475a
jioneee <qoxx3659@gmail.com>
2023-11-14 23:12:33 +0900

    Design: 로딩 애니메이션 적용

--- src/components/common/Loading.tsx
+++ src/components/common/Loading.tsx
@@ -27,4 +27,30 @@ const LoadingContainer = styled.div`
 const LoadingText = styled.div`
   font: 1rem 'Noto Sans KR';
   text-align: center;
+  width: 11px;
+  height: 11px;
+  border: 1.5px solid $gray-4;
+  border-top: 1.5px solid $gray-10;
+  border-radius: 50%;
+
+  -webkit-animation: spin 1s linear infinite;
+  animation: spin 1s linear infinite;
+
+  @-webkit-keyframes spin {
+    0% {
+      -webkit-transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
