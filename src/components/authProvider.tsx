025a6238a27363eed06b3e12e13e6d6a8319a758
jioneee <qoxx3659@gmail.com>
2024-02-22 22:19:35 +0900

    Feat: authProvider 생성

---
+++ src/components/authProvider.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react';
+import { User } from '@firebase/auth';
+import { useEffect, useState } from 'react';
+import { AuthContext } from '../context/authContext';
+import { auth } from '../firebase';
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const subscribe = auth.onAuthStateChanged((newUser) => {
+      console.log(`user`, newUser);
+      setUser(newUser);
+    });
+    return subscribe;
+  }, []);
+
+  return <AuthContext.Provider value={user}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
