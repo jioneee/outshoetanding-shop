5cda6cf79b2212115a4e569f17d6722e53244022
jioneee <qoxx3659@gmail.com>
2024-03-14 21:47:23 +0900

    Feat: api key 수정

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -5,7 +5,7 @@ import { getAuth } from 'firebase/auth';
 // For Firebase JS SDK v7.20.0 and later
 //measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: 'AIzaSyBH1BAJsQLfzsMgQ24Sax7jJMze78rzekY',
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
